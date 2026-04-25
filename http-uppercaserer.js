b005a5cd5fd208c5f6b516b627c7ca3b085b413e
Nura Mouktar <noormouktar@yahoo.com>
2026-04-24 19:55:24 -0700

    Update http-uppercaserer.js

--- http-uppercaserer.js
+++ http-uppercaserer.js
@@ -1,23 +1,28 @@
 // Import modules
+//loads Node.js HTTP module
 const http = require('http');
 
-// Create server
+// Create the web server
 http.createServer(function(req, res) {
 
+    //checks if client used POST
     if (req.method === 'POST') {
 
         let body = '';
 
+        //Node receives request data in chunks
         req.on('data', function(chunk) {
             body += chunk;
         });
 
+        //runs after it received all data
+        //then process the final text
         req.on('end', function() {
 
-            // Convert to uppercase
+            // Convert lowercase to uppercase
             res.end(body.toUpperCase());
 
         });
     }
-
+//uses command-line argument
 }).listen(process.argv[2]);
